29935878b5ca57497e8b6f0d3794afbdb929dca7
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-12 21:25:47 +0200

    added code to maximize window in IC

--- utils/LaunchBrowser.py
+++ utils/LaunchBrowser.py
@@ -3,6 +3,6 @@ from utils import config_properties
 def launch_browser(driver):
     dev_url = config_properties.ReadConfig_CommonDetails().getDevUrl()
     driver.get(dev_url)
-    driver.maximize_window()
-
+    # driver.maximize_window()
+    driver.set_window_size(1920, 1080)
     return driver
